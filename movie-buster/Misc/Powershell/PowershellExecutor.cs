3b690b678a7450d051483698104ca627d50abb3b
Tal Jacob <taljacob2@gmail.com>
2022-10-29 12:31:21 +0300

    Add `PowershellExecutor.cs`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

---
+++ movie-buster/Misc/Powershell/PowershellExecutor.cs
@@ -0,0 +1,24 @@
+﻿using System.Management.Automation;
+
+namespace movie_buster.Misc.Powershell
+{
+    public class PowershellExecutor
+    {
+        public PowershellExecutor() { }
+
+        public void run (string scriptPath, params string[] args)
+        {
+            using (PowerShell PowerShellInst = PowerShell.Create())
+            {
+                PowerShell ps = PowerShell.Create();
+
+                ps.AddScript(File.ReadAllText(scriptPath));
+
+                // Add arguments to the command.
+                foreach (string arg in args) { ps.AddArgument(arg); }
+
+                ps.Invoke();
+            }
+        }
+    }
+}
