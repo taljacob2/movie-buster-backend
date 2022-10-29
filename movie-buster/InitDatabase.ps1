44bafab80d0986e1c039f54cfde94b53c7e5e925
Tal Jacob <taljacob2@gmail.com>
2022-10-29 13:47:40 +0300

    Reorder Init Tables In `InitDatabase.ps1`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- movie-buster/InitDatabase.ps1
+++ movie-buster/InitDatabase.ps1
@@ -1,5 +1,5 @@
-sqlcmd -S DESKTOP-JJHPQ0B\SQLEXPRESS -i Sql\Init\Movies.sql
-sqlcmd -S DESKTOP-JJHPQ0B\SQLEXPRESS -i Sql\Init\MovieGenres.sql
-sqlcmd -S DESKTOP-JJHPQ0B\SQLEXPRESS -i Sql\Init\Tickets.sql
-sqlcmd -S DESKTOP-JJHPQ0B\SQLEXPRESS -i Sql\Init\TicketTypes.sql
 sqlcmd -S DESKTOP-JJHPQ0B\SQLEXPRESS -i Sql\Init\Customers.sql
+sqlcmd -S DESKTOP-JJHPQ0B\SQLEXPRESS -i Sql\Init\TicketTypes.sql
+sqlcmd -S DESKTOP-JJHPQ0B\SQLEXPRESS -i Sql\Init\MovieGenres.sql
+sqlcmd -S DESKTOP-JJHPQ0B\SQLEXPRESS -i Sql\Init\Movies.sql
+sqlcmd -S DESKTOP-JJHPQ0B\SQLEXPRESS -i Sql\Init\Tickets.sql
