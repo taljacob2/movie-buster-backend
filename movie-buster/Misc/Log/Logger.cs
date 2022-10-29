67acbad283f19af3e05c0550d6b1bc904cfacf65
Tal Jacob <taljacob2@gmail.com>
2022-10-29 13:19:34 +0300

    Add `Logger.cs`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

---
+++ movie-buster/Misc/Log/Logger.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace movie_buster.Misc.Log
+{
+    public class Logger
+    {
+
+        private static ILogger<Program> iLogger = InitILogger();
+
+        public Logger () { }
+
+        private static ILogger<Program> InitILogger()
+        {
+            ServiceProvider serviceProvider = new ServiceCollection()
+               .AddLogging((loggingBuilder) => loggingBuilder
+                   .SetMinimumLevel(LogLevel.Trace)
+                   .AddConsole()
+                   )
+               .BuildServiceProvider();
+
+            return serviceProvider.GetService<ILoggerFactory>().CreateLogger<Program>();
+        }
+
+        public void LogInfo(string str)
+        {
+            iLogger.LogInformation(str);
+        }
+
+        public void LogDebug(string str)
+        {
+            iLogger.LogDebug(str);
+        }
+    }
+}
